a043b431a0a535bdb9f5056817459f25a533e991
Asma Oualmakran <asmaoualmakran@gmail.com>
2017-11-28 12:07:00 +0100

    PUT and DELETE requests added for addresses, deleted old unused code

--- SpotEvent/SpotEventApp/controllers/address.py
+++ SpotEvent/SpotEventApp/controllers/address.py
@@ -22,24 +22,23 @@ def address_request(request):
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
-def create_address(request, pk):
-	try:
-		address = addressModel.objects.get(id=pk)
-	except:
-		return Response(serializer.errors, status=status.HTTP_400_NOT_FOUND)
-	serializer = addressSerializer(data=request.data)
-	if (serializer.is_valid()):
-		address = serializer.save()
-		serializer = addressSerializer(adress, context={'request':request})
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
-	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def single_address_request(request, pk): 
 	try: 
 		address = addressModel.objects.get(id=pk)
-	except: 
+	except addressModel.DoesNotExist: 
 		return Response(status=status.HTTP_404_NOT_FOUND)
-	serializer = addressSerializer(address, context={'request':request})
-	return Response(serializer.data)
+
+	if(request.method == 'GET'):	
+		serializer = addressSerializer(address, context={'request':request})
+		return Response(serializer.data, status=status.HTTP_200_OK)
+
+	elif(request.method == 'PUT'):
+		serializer = addressSerializer(address, data=request.data, context={'request':request})
+		if(serializer.is_valid()):
+			serializer.save()
+			return Response(status=status.HTTP_204_NO_CONTENT)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	else:
+		address.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
