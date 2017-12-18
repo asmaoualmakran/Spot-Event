55a696d3c1b87eed61429708c4d7fb7e0ce8271f
rcortvri <robin.frans.cortvrindt@vub.ac.be>
2017-12-18 12:53:31 +0100

    Added the profile and address page with dummys

--- SpotEvent/SpotEventApp/static/js/AddressModel.js
+++ SpotEvent/SpotEventApp/static/js/AddressModel.js
@@ -3,6 +3,28 @@ var viewModel = new function()
 {
     var self = this;
 
+    self.data;
+
+    self.dummy= {
+    	street : 'Vijverstraat',
+    	number : 27,
+    	zipcode : 1730,
+    	country : 'Belgium'
+
+    }
+    
+    self.street = ko.observable(self.dummy.street);
+    self.number = ko.observable(self.dummy.number);
+    self.zipcode = ko.observable(self.dummy.zipcode);
+    self.country = ko.observable(self.dummy.country);
+
+
+
+    self.getAddress = function(){
+    	console.log("getAddress")
+    	$.get('/api/address',self.data)
+    }
+
 }
 
 ko.applyBindings(viewModel);  
--- SpotEvent/SpotEventApp/static/js/ProfileModel.js
+++ SpotEvent/SpotEventApp/static/js/ProfileModel.js
@@ -1,16 +1,17 @@
 
 var viewModel = new function()
 {
-    var self.dummy = {
-    	userName = 'Thoraxis'
-    	firstName = 'Robin'
-    	lastName = 'Cortvrindt'
-    	email = 'cortvrindt.robin@gmail.com'
-    	birthdate = '28/11/95'
-    	password = '********'
+    var self = this;
+
+    self.dummy = {
+    	userName : 'Thoraxis',
+    	firstName : 'Robin',
+    	lastName : 'Cortvrindt',
+    	email : 'cortvrindt.robin@gmail.com',
+    	birthdate : '28/11/95',
+    	password : '*********',
     }
 
-    var self = this;
     self.userName = ko.observable(self.dummy.userName);
     self.firstName = ko.observable(self.dummy.firstName);
     self.lastName = ko.observable(self.dummy.lastName);
@@ -18,12 +19,16 @@ var viewModel = new function()
     self.birthdate = ko.observable(self.dummy.birthdate);
     self.password = ko.observable(self.dummy.password);
     
-    var self.data
-
+    self.data;
 
     self.getProfile = function(){
     	console.log('getProfile')
     	$.get('TODO : get profile', self.data)
+    }
+
+    self.saveProfile = function(){
+        console.log('saveProfile')
+        $.post('TODO: post profile', self.data)
     }
 
 }
