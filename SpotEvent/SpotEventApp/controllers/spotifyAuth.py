f72e6f08d8f94887d70a7fadb70dec36b935cbea
Asma Oualmakran <asmaoualmakran@gmail.com>
2017-12-20 16:05:03 +0100

    login spotify api added

--- SpotEvent/SpotEventApp/controllers/spotifyAuth.py
+++ SpotEvent/SpotEventApp/controllers/spotifyAuth.py
@@ -10,21 +10,15 @@ import base64, requests
 #to the autorize_link defined in the config
 @api_view(['GET'])
 def login(request):
-	print('start login')
 	return redirect(config.authorize_link)
 
 
 # create the authorisation link 
 @api_view(['GET'])
 def request_Auth(request):
-	print('test')
-	print(request)
 	code = request.GET.get('code') #we querry out the code, we need this for the post request back to Spotify API 
-	print('code fetched')
-	print(code)
-	request_link = requests.post(config.token_link, data={'code': code, 'grant_type': 'authorisation_code', 'redirect_uri': 'http://127.0.0.1:8080/authed', 'client_id': config.PUBLIC_KEY, 'client_secret': config.PRIVATE_KEY})
+	request_link = requests.post(config.token_link, data={'code': code, 'grant_type': 'authorization_code', 'redirect_uri': 'http://127.0.0.1:8000/authed', 'client_id': config.PUBLIC_KEY, 'client_secret': config.PRIVATE_KEY})
 	content = request_link.content
-	print(request_link.content)
 	return Response(content)
 
 #token_link, we send a request to this link 
